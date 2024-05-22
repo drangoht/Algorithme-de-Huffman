b136cdf352d1927b648cfc8c12a1b42641c32cb3
Grégory Thognard <drangoht@gmail.com>
2024-05-22 11:59:32 +0200

    serilog and exception catching in api

--- src/HuffmanWeb/HuffmanWeb.Server/Program.cs
+++ src/HuffmanWeb/HuffmanWeb.Server/Program.cs
@@ -1,8 +1,14 @@
 using HuffmanWeb.Algorithm;
 using HuffmanWeb.Server.DTOs;
+using Microsoft.AspNetCore.Http;
+using Serilog;
+
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateLogger();
 
 var builder = WebApplication.CreateBuilder(args);
-
+builder.Services.AddProblemDetails();
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowAll", builder =>
@@ -16,7 +22,18 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
-
+app.UseStatusCodePages(statusCodeHandlerApp =>
+{
+    statusCodeHandlerApp.Run(async httpContext =>
+    {
+        var pds = httpContext.RequestServices.GetService<IProblemDetailsService>();
+        if (pds == null
+            || !await pds.TryWriteAsync(new() { HttpContext = httpContext }))
+        {
+            await httpContext.Response.WriteAsync("Fallback: An error occurred.");
+        }
+    });
+});
 app.UseCors("AllowAll");
 
 app.UseDefaultFiles();
@@ -35,20 +52,30 @@ app.UseHttpsRedirection();
 
 app.MapPost("/huffman/encode", (EncodeRequest textToEncode) =>
 {
-    var huf = new Huffman();
-    huf.EncodeText(textToEncode.TextToEncode);
-    TextToEncodeResponse resp = new TextToEncodeResponse()
+    try
     {
-        Graph = huf.GeneratedGraph,
-        EncodedBinaryString = huf.TextEncoded,
-        EncodedSize = huf.InputHuffmanBinarySize,
-        OriginalSize = huf.InputBinarySize
-    };
-    foreach (var key in huf.HuffmanTable.Keys)
-    {
-        resp.MatchingCharacters.Add(new Character() { Id = key.ToString(), Value = huf.HuffmanTable[key].ToString() });
+        var huf = new Huffman();
+        huf.EncodeText(textToEncode.TextToEncode);
+        TextToEncodeResponse resp = new TextToEncodeResponse()
+        {
+            Graph = huf.GeneratedGraph,
+            EncodedBinaryString = huf.TextEncoded,
+            EncodedSize = huf.InputHuffmanBinarySize,
+            OriginalSize = huf.InputBinarySize
+        };
+        foreach (var key in huf.HuffmanTable.Keys)
+        {
+            resp.MatchingCharacters.Add(new Character() { Id = key.ToString(), Value = huf.HuffmanTable[key].ToString() });
+        }
+        return Results.Ok(resp);
     }
-    return TypedResults.Ok(resp);
+    catch (Exception e)
+    {
+        Log.Error(e.Message);
+        return Results.BadRequest();
+    }
+
+
 })
 .WithName("PostHuffmanEncode")
 .WithOpenApi();
