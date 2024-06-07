0825f19f35533393d2817edced4c86251e1b6e52
Grégory Thognard <drangoht@gmail.com>
2024-06-07 15:57:05 +0200

    design en cours

--- src/HuffmanWeb/HuffmanWeb.Mobile.Client/Pages/Decode/DecodeTabbedPage.xaml.cs
+++
@@ -1,9 +0,0 @@
-namespace HuffmanWeb.Mobile.Client.Pages.Decode;
-
-public partial class DecodeTabbedPage : TabbedPage
-{
-    public DecodeTabbedPage()
-    {
-        InitializeComponent();
-    }
-}
