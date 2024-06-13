d4523195059f87826d7cdc62389a9edf57be7d07
Grégory Thognard <drangoht@gmail.com>
2024-06-13 13:56:15 +0200

    espacement et polices

--- src/HuffmanWeb/HuffmanWeb.Mobile.Client/Platforms/Android/MainActivity.cs
+++ src/HuffmanWeb/HuffmanWeb.Mobile.Client/Platforms/Android/MainActivity.cs
@@ -3,7 +3,7 @@ using Android.Content.PM;
 
 namespace HuffmanWeb.Mobile.Client
 {
-    [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, LaunchMode = LaunchMode.SingleTop, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
+    [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ScreenOrientation = ScreenOrientation.Portrait, LaunchMode = LaunchMode.SingleTop, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
     public class MainActivity : MauiAppCompatActivity
     {
     }
