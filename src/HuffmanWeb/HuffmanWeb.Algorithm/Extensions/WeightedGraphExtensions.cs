91acb9af7c6452c66d9492ecbe91721a2297f587
Grégory Thognard <drangoht@gmail.com>
2025-09-06 23:17:02 +0200

    fix warning (#7)
    Co-authored-by: Drangoht <drangoht@gmail.co>

--- src/HuffmanWeb/HuffmanWeb.Algorithm/Extensions/WeightedGraphExtensions.cs
+++ src/HuffmanWeb/HuffmanWeb.Algorithm/Extensions/WeightedGraphExtensions.cs
@@ -10,16 +10,16 @@ namespace HuffmanWeb.Algorithm.Extensions
             foreach (var link in links)
             {
                 var nodeParent = graph.AllNodes.FirstOrDefault(n => n == link.Parent);
-                FillDescendantsCount(graph, nodeParent, link.Child);
+                FillDescendantsCount(graph, nodeParent);
 
             }
         }
-        private static void FillDescendantsCount(WeightedGraph graph, HuffmanNode? parentNode, HuffmanNode? childNode)
+        private static void FillDescendantsCount(WeightedGraph graph, HuffmanNode? parentNode)
         {
             if (parentNode == null) return;
             // Count direct descendantsNode + add sum of descendants node
             parentNode.DescendantsCount = graph.Links.Count(l => l.Parent == parentNode) + graph.Links.Where(l => l.Parent == parentNode).Sum(n => n.Child!.DescendantsCount);
-            FillDescendantsCount(graph, graph.Links.FirstOrDefault(n => n.Child == parentNode)?.Parent, parentNode);
+            FillDescendantsCount(graph, graph.Links.FirstOrDefault(n => n.Child == parentNode)?.Parent);
         }
     }
 }
